5edd8dcceb6dc34bfa3038d194c529285adda022
Roni 88 <ronix88x@hotmail.com>
2023-01-06 20:09:43 -0300

    ajustes gerais

--- src/index.js
+++ src/index.js
@@ -8,30 +8,40 @@ function hideCard () {
     selectedCard.classList.remove ('selected')
 }
 
-function showCard (indexCard) {
-    cards [indexCard].classList.add ('selected')
+function showCard () {
+    cards [showingCard].classList.add ('selected')
 }
 
-previous.addEventListener ('click', function () {
-    if (showingCard === 1) {
-        previous.classList.add (selected)
-    }
-    if (showingCard === cards.length -1) {
-        nextOne.classList.remove (selected)
-    }
-    hideCard ()
-    showingCard --
-    showCard (showingCard)
+nextOne.addEventListener ('click', function () {
+    previous.style.backgroundColor = 'blue'
 })
 
-nextOne.addEventListener ('click', function () {
-    if (showingCard === 0) {
-        previous.classList.remove (selected)
-    }
-    if (showingCard === cards.length -2) {
-        nextOne.classList.add (selected)
-    }
-    hideCard ()
-    showingCard ++
-    showCard (showingCard)
+previous.addEventListener ('click', function () {
+    nextOne.style.backgroundColor = 'blue'
 })
+
+
+
+// nextOne.addEventListener ('click', function () {
+//     if (showingCard === 0) {
+//         previous.classList.remove (selected)
+//     }
+//     if (showingCard === cards.length -2) {
+//         nextOne.classList.add (selected)
+//     }
+//     hideCard ()
+//     showingCard ++
+//     showCard (showingCard)
+// })
+
+// previous.addEventListener ('click', function () {
+//     if (showingCard === 1) {
+//         previous.classList.add (selected)
+//     }
+//     if (showingCard === cards.length -1) {
+//         nextOne.classList.remove (selected)
+//     }
+//     hideCard ()
+//     showingCard --
+//     showCard (showingCard)
+// })
