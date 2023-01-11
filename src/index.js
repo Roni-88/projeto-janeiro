eb489c6f0a8487a77381b06132372615eebcffff
Roni 88 <ronix88x@hotmail.com>
2023-01-11 20:04:26 -0300

    ajustes nos botões

--- src/index.js
+++ src/index.js
@@ -1,47 +1,32 @@
-const previous = document.getElementById ('prev')
-const nextOne = document.getElementById ('next')
+const previous = document.getElementById ('voltar')
+const nextOne = document.getElementById ('avancar')
 const cards = document.querySelectorAll ('.card')
 let showingCard = 0
 
 function hideCard () {
-    const selectedCard = document.querySelector ('.selected')
-    selectedCard.classList.remove ('selected')
+    let totalCards = 0
+    if (showingCard === totalCards){
+        return
+    }
+    showingCard = showingCard -1
+    cards.forEach(card => {
+        card.classList.remove ('selected')
+    });
+    cards[showingCard].classList.add ('selected')
 }
 
 function showCard () {
-    cards [showingCard].classList.add ('selected')
+    let totalCards = cards.length -1
+    if (showingCard === totalCards){
+        return
+    }
+    showingCard = showingCard +1
+    cards.forEach(card => {
+        card.classList.remove ('selected')
+    });
+    cards[showingCard].classList.add ('selected')
 }
 
-nextOne.addEventListener ('click', function () {
-    previous.style.backgroundColor = 'blue'
-})
+nextOne.addEventListener ('click', showCard)
 
-previous.addEventListener ('click', function () {
-    nextOne.style.backgroundColor = 'blue'
-})
-
-
-
-// nextOne.addEventListener ('click', function () {
-//     if (showingCard === 0) {
-//         previous.classList.remove (selected)
-//     }
-//     if (showingCard === cards.length -2) {
-//         nextOne.classList.add (selected)
-//     }
-//     hideCard ()
-//     showingCard ++
-//     showCard (showingCard)
-// })
-
-// previous.addEventListener ('click', function () {
-//     if (showingCard === 1) {
-//         previous.classList.add (selected)
-//     }
-//     if (showingCard === cards.length -1) {
-//         nextOne.classList.remove (selected)
-//     }
-//     hideCard ()
-//     showingCard --
-//     showCard (showingCard)
-// })
+previous.addEventListener ('click', hideCard)
